4870e965d53b2b517dc05401c435eda752ed5abb
Yunqi Guo <guoyunqi@gmail.com>
2021-06-23 16:56:15 -0700

    Show the gesture info

--- gestureControl.py
+++ gestureControl.py
@@ -68,6 +68,7 @@ with mp_hands.Hands(
         '''
         if results.multi_hand_landmarks:
             for hand_landmarks in results.multi_hand_landmarks:
+                print(hand_landmarks)
                 mp_drawing.draw_landmarks(
                     image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
         cv2.imshow('MediaPipe Hands', image)
